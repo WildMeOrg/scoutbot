ae9adfb1f9ffab4d9ed81482f9d4786967233428
LashaO <otarashvililasha@gmail.com>
2024-01-23 18:25:37 +0400

    fix linting

--- scoutbot/tile_batched/main.py
+++ scoutbot/tile_batched/main.py
@@ -41,7 +41,7 @@ class Yolov8DetectionModel(Yolov8DetectionModelBase):
         all_preds = []
         for i in range(0, len(images), batch_size):
             print(i)
-            batch_images = images[i:i+batch_size]
+            batch_images = images[i:i + batch_size]
             preds = self.model.predict(source=batch_images, verbose=False, device=self.device)
 
             all_preds.extend(preds)
